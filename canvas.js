2276bd5804f31cc3ad00eb9064f1ebeec2a17624
xylo <devyarustagi@gmail.com>
2026-03-24 00:41:07 +0530

    feature: basic drawing facilities

--- canvas.js
+++ canvas.js
@@ -3,7 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 /*add canvas resizing feature, store all canvas objects in an array*/
-
+let is_drawing = false;
+let startX = 0;
+let startY = 0;
 let curr_tool = localStorage.getItem("curr_tool");
 if (curr_tool === "") {
     localStorage.setItem("curr_tool", "selection");
@@ -38,3 +40,63 @@ for (const tool of tools) {
     });
 }
 
+//drawing functions :
+function draw_line(endX,endY) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+}
+function draw_rectangle(endX,endY) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.strokeRect(startX,startY,endX-startX,endY-startY);
+}
+
+function draw_ellipse(endX, endY) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.beginPath();
+    ctx.ellipse((startX+endX)/2, (endY+startY)/2,Math.abs((endX-startX)/2),Math.abs((endY-startY)/2),0,0,2*Math.PI);
+    ctx.stroke();
+}
+
+function draw_free(endX, endY) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
+    ctx.stroke();
+    startX = endX;
+    startY = endY;
+}
+
+canvas.addEventListener("mousedown", (event) => {
+    is_drawing = true;
+    startX = event.clientX;
+    startY = event.clientY;
+})
+canvas.addEventListener("mousemove", (event) => {
+    if (is_drawing === true) {
+        if(curr_tool === "line"){
+                draw_line(event.clientX, event.clientY);
+        }
+        else if(curr_tool === "draw_rectangle"){
+                draw_rectangle(event.clientX, event.clientY);
+        }
+        else if(curr_tool === "draw_circle"){
+                draw_ellipse(event.clientX, event.clientY);
+        }
+        else if(curr_tool === "pencil"){
+                draw_free(event.clientX, event.clientY);
+        }
+
+    }
+})
+canvas.addEventListener("mouseup", (event) => {
+    is_drawing = false;
+})
+
+//to prevent glitches when mouse leaves canvas
+canvas.addEventListener("mouseleave", (event) => {
+    is_drawing = false;
+})
+//bug to be fixed later : drawing stops when pointer crosses toolbar
