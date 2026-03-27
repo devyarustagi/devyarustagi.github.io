f070a929596dac29c89fd3ee57ee0b4d847e9b44
xylo <devyarustagi@gmail.com>
2026-03-28 00:28:17 +0530

    feat: User controls the color of the canvas and it is theme independent.

--- toggle_theme.js
+++ toggle_theme.js
@@ -1,8 +1,14 @@
 let theme = localStorage.getItem('light')
 const themeSwitch = document.getElementById('toggle_theme')
-
-const enableLightmode = () => {
-    document.documentElement.style.setProperty('--bg-color', '#cbcbcb');
+if(!localStorage.getItem("canvas_color")){
+    localStorage.setItem("canvas_color","#222831")
+}
+document.documentElement.style.setProperty('--bg-color', `${localStorage.getItem("canvas_color")}`);
+document.getElementById("canvas_color").addEventListener("change",(e)=>{
+    localStorage.setItem("canvas_color",`${e.currentTarget.value}`)
+    document.documentElement.style.setProperty('--bg-color', `${e.currentTarget.value}`);
+})
+const enableLightmode = () =>{
     document.documentElement.style.setProperty('--button-background-color', '#ffffe3');
     document.documentElement.style.setProperty('--button-highlight-color', 'black');
     document.documentElement.style.setProperty('--button-fill-color', '#4a4a4a');
@@ -12,7 +18,6 @@ const enableLightmode = () => {
 }
 
 const disableLightmode = () => {
-    document.documentElement.style.setProperty('--bg-color', '#222831');
     document.documentElement.style.setProperty('--button-background-color', '#393E46');
     document.documentElement.style.setProperty('--button-highlight-color', '#DFD0B8');
     document.documentElement.style.setProperty('--button-fill-color', 'white');
